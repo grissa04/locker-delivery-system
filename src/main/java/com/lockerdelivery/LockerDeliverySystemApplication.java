cff3992e542498afd6e8e0c058b7c0a57d50a27d
rayenrejeb <rayen.rejeb@gmail.com>
2026-04-17 00:17:47 +0200

    init project

---
+++ src/main/java/com/lockerdelivery/LockerDeliverySystemApplication.java
@@ -0,0 +1,12 @@
+package com.lockerdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerDeliverySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockerDeliverySystemApplication.class, args);
+    }
+}
---
+++ src/test/java/com/lockerdelivery/LockerDeliverySystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lockerdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class LockerDeliverySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
